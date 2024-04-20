d03aa39f07881ea9f5723bb9be634b14b2a7509a
Victor Zapata <victorzapata@MBP-de-Victor.fibertel.com.ar>
2024-04-20 14:06:28 -0300

    styles-formlist

--- src/components/FormList.tsx
+++ src/components/FormList.tsx
@@ -12,7 +12,7 @@ const FormList = ({ activitiesForm }: FormListProps) => {
                 activitiesForm.map((item) => {
                     return <div className="px-5 py-10 bg-white mt-5 flex justify-between" key={item.id}>
                         <div className="space-y-2 relative">
-                            <p>{item.categorie}</p>
+                            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${item.categorie === 'Comida' ? "bg-lime-500" : " bg-orange-500"}`}>{item.categorie}</p>
                             <p className="text-2xl font-bold pt-5">{item.activities}</p>
                             <p className="font-black text-4xl text-lime-500">
                                 {item.calories}{' '}
